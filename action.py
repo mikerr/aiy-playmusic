89428a1b5d9cdca3fcfe06182d925b59ae6f5d03
Mike Redrobe <mike@redrobe.net>
2017-05-08 11:19:54 +0100

    Create action.py

---
+++ action.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as gpio
+import time
+
+# =========================================
+# Makers! Implement your own actions here.
+# =========================================
+
+
+class play(object):
+
+    def __init__(self, say, keyword):
+        self.say = say
+        self.keyword = keyword
+
+    def run(self, voice_command):
+
+        track = voice_command.replace(self.keyword, '', 1)
+
+        p = subprocess.Popen(["/usr/local/bin/mpsyt",""],stdin=subprocess.PIPE)
+
+        p.stdin.write(bytes('/' + track + '\n1\n', 'utf-8'))
+        p.stdin.flush()
+
+        gpio.setmode(gpio.BCM)
+        gpio.setup(23, gpio.IN)
+
+        while ( gpio.input(23) ):
+             time.sleep(1)
+
+        p.stdin.write(bytes('exit\n', 'utf-8'))
+        p.stdin.flush()
+        
+        
+    # =========================================
+    # Makers! Add your own voice commands here.
+    # =========================================
+
+actor.add_keyword(_('play'), play(say,_('play')))
